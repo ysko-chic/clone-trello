e65267d4d91f84edd923d8064e1a48601d133c07
ysko-chic <kys0916@nate.com>
2021-07-20 17:58:04 +0900

    헤더 이미지 추가

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -42,10 +42,6 @@ class Card {
         cardDiv.onmouseover = this.mouseOverHandler;
         cardDiv.ondragenter = this.dragEnterHandler;
         cardDiv.ondragleave = this.dragLeaveHandler;
-
-        cardText.onclick = (e) => {
-            e.stopPropagation();
-        }
     }
 
     cardTextOnFocusOut = () => {
